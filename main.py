a57dc752c08c88b6d8ad84e1ab658d29e7288cdc
chencheng <chencheng03642@klicen.com>
2022-02-17 16:59:37 +0800

    update

--- main.py
+++ main.py
@@ -4,16 +4,38 @@
 # @Author    :ChenCheng
 
 from typing import Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
 @app.get("/")
-def index():
+async def index():
     return {"hello": "world"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/item/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/items/")
+async def search_item(page: int = 0, query: str = ''):
+    return {"page": page, "query": query}
+
+
+@app.post("/items/")
+async def create_items(item: Item):
+    return item
+
+
+if __name__ == '__main__':
+    pass
